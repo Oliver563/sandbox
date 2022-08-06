b8a0e35e253234654f74e006e5fa35163ab2be9d
DESKTOP-00L36LH\willi <willisgra@googlemail.com>
2022-08-06 16:17:25 +0100

    added post handler

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from ast import Dict
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from jinja2 import Environment, PackageLoader, select_autoescape
 
@@ -7,15 +8,26 @@ env = Environment(
 
 hostName = "localhost"
 serverPort = 8080
-
+save_data = dict( name = "" )
 template = env.get_template("sandbox-template.html")
 
 class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
+    def _set_response(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(bytes(template.render(name="Oliver"), "utf-8"))
+        
+    def do_GET(self):
+        self._set_response()
+        self.wfile.write(bytes(template.render(name=save_data['name']), "utf-8"))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) 
+        post_data = self.rfile.read(content_length) 
+        self.send_response(303)
+        self.send_header('Location', self.path)
+        self.end_headers()
+        save_data['name'] = post_data.decode('utf-8').split('=')[1]
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
