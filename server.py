7590fe5afcad1f4341e9db730f050878cf24c0d3
Oliver563 <oliverwillis563@gmail.com>
2022-09-06 19:16:26 +0100

    finished

--- server.py
+++ server.py
@@ -10,12 +10,12 @@ save_data = [{ "c1": ("", "tile"), "c2": ("", "tile"), "c3" : (
 ]
 
 current_path = os.path.dirname(__file__)
-f = open(current_path + "\static\wordle.css", "r")
-static_content = bytes(f.read(), "utf-8")
+f = open(current_path + "\static\wordle.css", "r") # opens the css style sheet on read
+static_content = bytes(f.read(), "utf-8") #sets static_content as the css style sheet
 f.close()
 
-word = (random.choice(open("words.txt").read().split()))
-word = word.upper()
+word = (random.choice(open("words.txt").read().split())) # gets a random word out of the word list
+word = word.upper() # sets the random word to uppercase
 print(word)
 
 current_row = 0
@@ -23,49 +23,49 @@ answer = ''
 
 def save_data_to_dict(d):
     global current_row
-    values = d.split('&')
+    values = d.split('&') # removes the & from the data
     for value in values:
-        row = int(value[1:2])
+        row = int(value[1:2]) # sets the row and the col to the appropriate values within the data
         col = 'c' + value[3:4]
-        value = value.split('=')[1]
+        value = value.split('=')[1] # removes the = from the data
         if (row == current_row + 1):
-            save_data[row - 1][col] = value
+            save_data[row - 1][col] = value # saves the data to the dict at the end of the row
 
     evaulate()
 
-    if (current_row == 5):
+    if (current_row == 5): # the lose condition
         global answer
-        answer = 'Bad luck, the word was ' + word
+        answer = 'Bad luck, the word was ' + word 
 
-    if (answer == ''):
+    if (answer == ''): 
         save_data.append({ "c1": ("", "tile"), "c2": ("", "tile"), "c3" : (
         "", "tile"), "c4": ("", "tile"),"c5": ("", "tile") })
-        current_row += 1
+        current_row += 1 # moves the current row down and sets the data of the row to nothing
 
-def evaulate():
+def evaulate(): # checks the letter to the word and updates the letters status
     position = 0
     global current_row
     found = 0
     for letter in save_data[current_row].values():
-        if letter[0].upper() == word[position:position+1]:
+        if letter[0].upper() == word[position:position+1]: # is the letter in the correct place in the word?
             print("foundit!")
             found += 1
             col = 'c' + str(position + 1)
-            save_data[current_row][col] = (letter[0].upper(), 'correct')
+            save_data[current_row][col] = (letter[0].upper(), 'correct') # sets the status of the letter
 
-        elif (word.__contains__(letter[0].upper())):
+        elif (word.__contains__(letter[0].upper())): # if the word contains the letter but not in the right place
             print("Found it but not here!")
             col = 'c' + str(position + 1)
             save_data[current_row][col] = (letter[0].upper(), 'present')
 
-        else:
+        else: # if the letter is not found within the word
             print("Not found")
             col = 'c' + str(position + 1)
             save_data[current_row][col] = (letter[0].upper(), 'absent')
 
         position += 1
 
-    if(found == 5):
+    if(found == 5): # the win condition
         global answer
         answer = 'Congratulations!'
 
@@ -75,13 +75,13 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_header('Content-type', type)
         self.end_headers()
 
-    def do_GET(self):
+    def do_GET(self): #gets information from server
         global static_content
         path = self.path
         if path == "/static/wordle.css":
-            type = "text/css"
+            type = "text/css" # sets the appropriate content type to each of the files
             self._set_response(type)
-            self.wfile.write(static_content)
+            self.wfile.write(static_content) 
         elif path == "/favicon.ico":
             type = "image/x-icon"
             self._set_response(type)
@@ -90,10 +90,10 @@ class MyServer(BaseHTTPRequestHandler):
             self._set_response(type)
             self.wfile.write(bytes(template.render(data=save_data, answer=answer), "utf-8"))
 
-    def do_POST(self):
+    def do_POST(self): #sends information to server
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        self.send_response(303)
+        self.send_response(303) 
         self.send_header('Location', self.path)
         self.end_headers()
         global answer 
